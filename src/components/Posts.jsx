9619441f4531591eb151fc537b8ed9a138dc6120
Husnain Raza <husnainraza.m51@gmail.com>
2024-06-13 10:28:18 +0500

    done

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { app } from "../firebase";
+import Post from "./Post";
 
-export default function Posts() {
-  return <div>Posts</div>;
+export default async function Posts() {
+  const db = getFirestore(app);
+  const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+  const querySnapshot = await getDocs(q);
+  let data = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return (
+    <div>
+      {data.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
