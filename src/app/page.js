09a0c694769f40c2ce720296cc9d623c8915a3f8
Husnain Raza <husnainraza.m51@gmail.com>
2024-06-13 09:03:20 +0500

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import Feed from "@/components/Feed";
 import React from "react";
 
 export default function Home() {
-  return <div className="text-red-300">Home</div>;
+  return (
+    <div className="">
+      <Feed />
+    </div>
+  );
 }
